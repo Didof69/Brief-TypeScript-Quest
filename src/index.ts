205e1165402b2b3998cd3034b9f1d48cc2f989b8
Lydie Chevalier <didof69@hotmail.fr>
2023-06-30 19:45:42 +0200

    bouton fight safe, on doit choisir les heros d'abord

--- src/index.ts
+++ src/index.ts
@@ -139,7 +139,7 @@ let hero1 = document.getElementById("hero1Choice") as HTMLSelectElement;
 let hero2 = document.getElementById("hero2Choice") as HTMLSelectElement;
 let heros1: Hero;
 let heros2: Hero;
-
+let debutPartie: boolean = false;
 // PARAMETRAGE DES HEROS //
 function chooseHero(): void {
   if (hero1.value == "heroAxe") {
@@ -160,7 +160,7 @@ function chooseHero(): void {
 
 chooseBtn.addEventListener("click", () => {
   chooseHero();
-
+  debutPartie = true;
   if (hero1.value == "heroAxe") {
     imgHero1.src = "./images/axe.jpg";
     nameHero1.innerHTML = `${jean.getName()}`;
@@ -192,41 +192,46 @@ chooseBtn.addEventListener("click", () => {
 fightBtn.addEventListener("click", () => {
   chooseHero();
 
-  if (hero1.value == "heroAxe") {
-    heros1 = jean;
-  } else if (hero1.value == "heroSpear") {
-    heros1 = paul;
+  if (!debutPartie) {
+    winner.innerText = `Choose Heroes before !`;
   } else {
-    heros1 = pierre;
-  }
-  if (hero2.value == "heroAxe") {
-    heros2 = jean;
-  } else if (hero2.value == "heroSpear") {
-    heros2 = paul;
-  } else {
-    heros2 = pierre;
-  }
 
-  // BOUCLE DU JEU //
-  let numberOfRound: number = 0;
-  let result: string = "";
-  while (heros1.isAlive() && heros2.isAlive()) {
-    heros1.attack(heros2);
-    heros2.attack(heros1);
-    console.log("la vie du héros 1 " + heros1.getLife());
-    console.log("la vie du héros 2 " + heros2.getLife());
-    numberOfRound++;
-    if (heros1.getLife() <= 0 && heros2.getLife() <= 0) {
-      result = `It's a draw !`;
-    } else if (heros1.getLife() <= 0) {
-      result = `Hero ${heros2.getName()} wins !`;
-    } else if (heros2.getLife() <= 0) {
-      result = `Hero ${heros1.getName()} wins !`;
+    if (hero1.value == "heroAxe") {
+      heros1 = jean;
+    } else if (hero1.value == "heroSpear") {
+      heros1 = paul;
+    } else {
+      heros1 = pierre;
     }
-  }
-  nbRound.innerText = `${numberOfRound}`;
-  console.log(nbRound);
+    if (hero2.value == "heroAxe") {
+      heros2 = jean;
+    } else if (hero2.value == "heroSpear") {
+      heros2 = paul;
+    } else {
+      heros2 = pierre;
+    }
 
-  winner.innerText = `${result}`;
-  console.log(result);
+    // BOUCLE DU JEU //
+    let numberOfRound: number = 0;
+    let result: string = "";
+    while (heros1.isAlive() && heros2.isAlive()) {
+      heros1.attack(heros2);
+      heros2.attack(heros1);
+      console.log("la vie du héros 1 " + heros1.getLife());
+      console.log("la vie du héros 2 " + heros2.getLife());
+      numberOfRound++;
+      if (heros1.getLife() <= 0 && heros2.getLife() <= 0) {
+        result = `It's a draw !`;
+      } else if (heros1.getLife() <= 0) {
+        result = `Hero ${heros2.getName()} wins !`;
+      } else if (heros2.getLife() <= 0) {
+        result = `Hero ${heros1.getName()} wins !`;
+      }
+    }
+    nbRound.innerText = `${numberOfRound}`;
+    console.log(nbRound);
+
+    winner.innerText = `${result}`;
+    console.log(result);
+  }
 });
